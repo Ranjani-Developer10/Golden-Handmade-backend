d03bcd08ecb54b6a1a09225aaa6189ee4dcdd684
SWATHI N <ranjaniram81@gmail.com>
2026-09-22 00:03:19 +0530

    Use Render dynamic port

--- index.js
+++ index.js
@@ -34,5 +34,9 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
 
